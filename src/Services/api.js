1c9223746950629e424a0eabebe0e2306529f50f
SnehaMgithub <snehamathes2000@gmail.com>
2024-11-23 20:23:34 +0530

    Updated

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://flightbooking-backend-yw36.onrender.com/api",
 });
 
 api.interceptors.request.use(
